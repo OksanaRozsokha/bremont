02d07c72023585dad1233cdbbef8ced09be3f99e
Oksana Rozsokha <o.rozsokha@gmail.com>
2017-09-19 16:19:52 +0300

    add product items to the page

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,30 @@ $(document).ready(function() {
         data: { get_param: 'value' },
         url: ' https://shop.bremont.com/products.json',
         dataType : "json",
-        success: function (data) {
+        success: function (data) { //you need to append the data to gridview
 
-            console.log(data.products[0].title);
+            $.each(data.products, function(index, product) {
 
+                const productItem = $(".products-list li");
+                $(".products-list").append("<li></li>");
+
+                productItem.addClass('product-element');
+
+
+                const image = document.createElement('img');
+                image.src = product.images[0].src;
+
+                $('.product-item  img').attr('src', product.images[0].src);
+
+                productItem.append(image);
+                productItem.append("<h2>" + product.title + "</h2>");
+                productItem.append("<p>" + product.body_html + "</p>");
+                productItem.append("<span>" + '\u00A3' + product.variants[0].price +"</span>");
+
+            });
         }
     });
+
 
 
 
