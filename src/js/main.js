f3a90ed2f9ab7f2b70629b8f4347ea2c90b17726
Oksana Rozsokha <o.rozsokha@gmail.com>
2017-09-21 20:45:08 +0300

    add bucketList

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,86 @@
+let bucketList = [];
+let products;
 let productInfo = document.getElementById('info-card');
 let ourRequest = new XMLHttpRequest();
 ourRequest.open( 'GET','https://shop.bremont.com/products.json');
 ourRequest.onload = function () {
     let ourData = JSON.parse(ourRequest.responseText);
-    // console.log(ourData);
-    renderHTML(ourData.products);
+    products = ourData.products;
+    renderHTML(products);
 };
 ourRequest.send();
 
 function renderHTML(products) {
     let htmlString = "";
-    console.log(products.length);
+    // console.log(products.length);
 
     for (i = 0; i < products.length; i++) {
-        console.log(i);
         htmlString += "<li class='col-md-6 col-lg-4'>";
-        htmlString += "<a href='#'><h2>" + products[i].title + "</h2></a>";
-        htmlString += sliderImages(products[i].images);
-        htmlString += "<p>" + products[i].body_html + "</p>";
-        htmlString += "<span class='price'>" + products[i].variants[0].price + "</span>";
+        htmlString += sliderImages([products[i].images[0]]);
+        htmlString += "<a href='#'><h2 class='title'>" + products[i].title + "</h2></a>";
+        htmlString += "<button class='buy-btn'  data-index='"+i+"'>Buy</button>";
+        htmlString += "<p class='text text-info'>" + products[i].body_html + "</p>";
+        htmlString += "<span class='text text-small text-grey'>" + products[i].variants[0].price + "</span>";
         htmlString +="</li>";
     }
-    console.log(htmlString);
+    // console.log(htmlString);
     productInfo.insertAdjacentHTML('beforeend', htmlString);
+    initButtons();
 }
 
 function sliderImages(images) {
-   let html = "";
+    let html = "";
     html += "<div class=''>";
-     if (images.length > 0) {
-         for (count = 0; count < images.length; count++) {
-             html += "<img class='slider-image' src='"+ images[count].src +"'>";
-         }
-     } else {
-         html += "<img class='slider-image' src='img/product-placeholder.jpg'>";
-     }
-     html += "</div>";
+    if (images.length > 0) {
+        for (count = 0; count < images.length; count++) {
+            html += "<img class='slider-image' src='"+ images[count].src +"'>";
+        }
+    } else {
+        html += "<img class='slider-image' src='img/product-placeholder.jpg'>";
+    }
+    html += "</div>";
 
-     return html;
+    return html;
 }
+function initButtons() {
+    let classname = document.getElementsByClassName('buy-btn');
+
+    let  myFunction = function() {
+        let attribute = this.getAttribute("data-index");
+        let product = products[attribute];
+
+        let bucketElement = {
+            id: product.id,
+            quantity: 1,
+            price: product.variants[0].price
+        };
+        console.log(bucketElement);
+
+        bucketList.push({
+            key:   product.id,
+            value: bucketElement
+        });
+
+        // bucketList.push({
+        //     key:   product.id,
+        //     value: bucketElement
+        // });
+
+        // bucketList.id = bucketElement.id;
+
+
+        console.log(bucketList);
+    };
+
+
+
+    for (let i = 0; i < classname.length; i++) {
+        classname[i].addEventListener('click', myFunction, false);
+    }
+}
+
+
+
 
 
 
---
+++ src/js/slider.js
@@ -0,0 +1,10 @@
+(function($) {
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        dots: true,
+        dotData: true,
+        dotsEach: true
+    });
+})(jQuery);
+
